b4194acc15df740bc389bcf6ed88e29f113c3633
Jhonny <jhonnattanriverarivera@gmail.com>
2020-05-31 00:10:00 -0400

    up first version of module-connector

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("test")
+'use strict'
+/**
+ * @version 1.0.0
+ * @author Jhonnattan Rivera
+ * v1.0.0 creation
+ * @description 
+ * object used to connect different backend and frontend components.
+ */
+class moduleConnector {
+
+    constructor() {
+
+        /** @boolean */
+        this.success = false;
+
+        /** @string @max_100 description of response */
+        this.message = "module_connector";
+
+        /** @obj || @array content of response*/
+        this.response = [];
+
+        /** @integer HTTP code response */ 
+        this.code = 500;
+
+        Object.preventExtensions(this);
+    };
+    
+}
+
+module.exports = moduleConnector;
