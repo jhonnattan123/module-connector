d01de910fa50b3be38244c3935261f38923eaafb
Jhonny <jhonnattanriverarivera@gmail.com>
2020-06-01 10:01:46 -0400

    new methods for reduce code

--- index.js
+++ index.js
@@ -24,7 +24,59 @@ class moduleConnector {
 
         Object.preventExtensions(this);
     };
-    
+
+    writeMessageAndReponse( response, message) {
+        if( typeof response == "string" && typeof message != "string" ) {
+            message = response;
+        } else {
+            this.response = response;
+        };
+        if( typeof message != "undefined"){
+            this.message = message;
+        }
+        return this
+    };
+
+    successFunctions ( response, message, code ) {
+        this.code = code;
+        this.success = true;
+        return this.writeMessageAndReponse( response, message);
+    };
+
+    failedFunctions ( response, message, code ) {
+        this.code = code;
+        this.success = false;
+        return this.writeMessageAndReponse( response, message);
+    };
+
+    ok ( response, message) {
+        return this.successFunctions( response, message, 200);
+    };
+
+    created ( response, message) {
+        return this.successFunctions( response, message, 201);
+    };
+
+    badRequest ( response, message) {
+        return this.failedFunctions( response, message, 400);
+    };
+
+    unauthorized ( response, message) {
+        return this.failedFunctions( response, message, 401);
+    };
+
+    forbidden ( response, message) {
+        return this.failedFunctions( response, message, 403);
+    };
+
+    notFound ( response, message) {        
+        return this.failedFunctions( response, message, 404);
+    };
+
+    conflict ( response, message) {        
+        return this.failedFunctions( response, message, 409);
+    };
+
 }
 
 module.exports = moduleConnector;
